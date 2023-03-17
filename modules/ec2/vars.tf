104a961b0c35230b4e66fa51517acbe4f6e9f405
mbahsamuel <snjei@yahoo.com>
2023-03-17 18:22:51 -0400

    first commit

---
+++ modules/ec2/vars.tf
@@ -0,0 +1,15 @@
+
+
+  variable "ec2_count" {
+      default = "1"
+  }
+
+  variable "ami_id" {
+      default = "ami-005f9685cb30f234b"
+  }
+
+    variable "instance_type" {
+      default = "t2.micro"
+  }
+
+    variable "subnet_id" {}
---
+++ prod/main.tf
@@ -0,0 +1,16 @@
+module "my_vpc" {
+    source = "../modules/vpc"
+    vpc_cidr = "192.167.0.0/16"
+    tenancy  =   "default"
+    vpc_id =  "${module.my_vpc.vpc_id}"
+    subnet_cidr = "192.167.0.0/24"
+}
+
+module "my_ec2" {
+    source = "../modules/ec2"
+    ec2_count = "1"
+    ami_id =  "ami-005f9685cb30f234b"
+    instance_type = "t2.medium"
+    subnet_id = "${module.my_vpc.subnet_id}"
+  
+}
